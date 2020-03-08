febcfe117ea280ea58f3156ed98ca59bebc26fb1
Metoraf007 <36585140+Metoraf007@users.noreply.github.com>
2020-03-08 14:06:38 +0200

    master

--- server.py
+++ server.py
@@ -6,5 +6,36 @@ from urllib.request import urlopen
 app = Flask(__name__)
 
 @app.route('/')
+def root():
+    return render_template('index.html')
+
+@app.route('/index.html')
 def index():
     return render_template('index.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/work.html')
+def work():
+    return render_template('work.html')
+
+@app.route('/works.html')
+def works():
+    return render_template('works.html')
+
+
+@app.route('/components.html')
+def components():
+    return render_template('components.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
