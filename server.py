fb99a870eb7c7819b53fbbb7df260f74e2d6a8e6
Metoraf007 <36585140+Metoraf007@users.noreply.github.com>
2020-05-09 14:01:29 +0300

    master

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request, render_template, send_from_directory
+from flask import Flask, escape, request, render_template, send_from_directory, redirect
 from os import path
 import json
 from urllib.request import urlopen
@@ -9,11 +9,20 @@ app = Flask(__name__)
 def root():
     return render_template('index.html')
 
-@app.route('/<page>')
-def return_page(page):
-    return render_template(page)
+@app.route('/<string:page_name>')
+def return_page(page_name):
+    return render_template(page_name)
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        email = data['email']
+        return redirect('/thankyou.html', <string:email>)
+    else:
+        return render_template('index.html')
