0c4304dd77cce287c1b9543549eea747521b490e
Metoraf007 <36585140+Metoraf007@users.noreply.github.com>
2020-05-09 13:39:22 +0300

    dynamic page requests

--- server.py
+++ server.py
@@ -9,31 +9,9 @@ app = Flask(__name__)
 def root():
     return render_template('index.html')
 
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
-
-@app.route('/contact.html')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
-
-@app.route('/work.html')
-def work():
-    return render_template('work.html')
-
-@app.route('/works.html')
-def works():
-    return render_template('works.html')
-
-
-@app.route('/components.html')
-def components():
-    return render_template('components.html')
-
+@app.route('/<page>')
+def return_page(page):
+    return render_template(page)
 
 @app.route('/favicon.ico')
 def favicon():
