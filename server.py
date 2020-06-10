fbb6359a4d3a7c25630f2da72a703076cfa41458
Rotems <metoraf.berserk@gmail.com>
2020-06-10 15:59:18 +0200

    upload

--- server.py
+++ server.py
@@ -1,15 +1,45 @@
-from flask import Flask, escape, request, render_template, send_from_directory, redirect
+from flask import Flask, flash, escape, request, render_template, send_from_directory, redirect, url_for
 from os import path
 import csv
 import json
 from urllib.request import urlopen
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'C:\\Users\\Rotems\\Desktop\\Github\\WebSiteMP\\uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def root():
     return render_template('index.html')
 
+@app.route('/upload', methods=['POST','GET'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('/thankyou.html')
+    return render_template('/upload.html')
+    
 @app.route('/<string:page_name>')
 def return_page(page_name):
     if page_name == 'thankyou.html':
@@ -40,3 +70,4 @@ def write_to_csv(data):
         message = data['message']
         csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email, subject, message])
+
